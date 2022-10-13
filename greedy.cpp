b7a6c3a0ab5c76354faa912875e0ec334f24f57c
GordonHoklinder <ondra.sladky@gmail.com>
2022-10-13 15:44:01 +0200

    Aho-Corasick Automaton for linear GREEDY implementation.

--- greedy.cpp
+++ greedy.cpp
@@ -3,34 +3,14 @@
 #include <iostream>
 #include <queue>
 #include <unordered_set>
+#include <list>
+#include <sstream>
 
 struct KMer {
 	std::string value;
 
 	size_t length() { return value.size(); }
 };
-
-/// Find the length of longest overlap in O(length^2).
-int OverlapLength(KMer first, KMer second) {
-	for (int start = 0; start < first.length(); ++start) {
-		bool overlaps = true;
-		for (int i = 0; i < std::min(first.length() - start, second.length()); ++i) {
-			if (first.value[start + i] != second.value[i]) {
-				overlaps = false;
-				break;
-			}
-		}
-		if (overlaps) return first.length() - start;
-	}
-	return 0;
-}
-
-/// Construct the union of two k-mers in O(length).
-KMer Union(KMer first, KMer second, int overlapLength) {
-	return KMer{
-		first.value + second.value.substr(overlapLength, second.length() - overlapLength)
-	};
-}
 
 struct KMerSet {
 	std::string superstring;
@@ -44,62 +24,161 @@ struct UnionEvent {
 	int overlapLength;
 };
 
-/// Get the approximated shortest superstring of the given k-mers using the GREEDY algorithm.
-/// This runs in O(n^2 o^2) where o is the size of the output (n <= o <= nk).
-KMerSet Greedy(std::vector<KMer> input) {
-	if (input.size() == 0) {
-		throw new std::invalid_argument("input cannot be empty");
-	}
-	int k = input[0].length();
-
-	auto kMers = std::vector<KMer>(input);
-	// There will be in total n - 1 new k-mers.
-	std::vector<bool> used (2 * kMers.size() - 1);
-	auto compare = [](UnionEvent &a, UnionEvent &b) {return a.overlapLength < b.overlapLength;};
-	std::priority_queue<UnionEvent, std::vector<UnionEvent>, decltype(compare)> q(compare);
-	// Find the overlaps of the initial k-mers.
-    for (int i = 0; i < kMers.size(); ++i) {
-		for (int j = 0; j < kMers.size(); ++j) if (j != i) {
-			q.push(UnionEvent{i, j, OverlapLength(kMers[i], kMers[j])});
-		}
-	} 
-
-    // Unite the k-mers hungrily.
-	// After n - 1 unions, all k-mers are united.
-	while (kMers.size() != 2 * input.size() - 1) {
-		auto next = q.top();
-		q.pop();
-		// If none of the individual strings have yet been merged, merge them.
-		if (!used[next.firstIndex] && !used[next.secondIndex]) {
-			used[next.firstIndex] = used[next.secondIndex] = true;
-			KMer united = Union(kMers[next.firstIndex], kMers[next.secondIndex], next.overlapLength);
-            // Get the overlap of the newly created string with the existing ones.
-            for (int i = 0; i < kMers.size(); ++i) if (!used[i]) {
-				q.push(UnionEvent{i, (int)kMers.size(), OverlapLength(kMers[i], united)});
-				q.push(UnionEvent{(int)kMers.size(), i, OverlapLength(united, kMers[i])});
-			}
-			kMers.push_back(united);
-		}
-	}
-
-	auto resultValue = kMers[kMers.size() - 1].value;
-	KMerSet result = KMerSet {
-		resultValue,
-		std::vector<bool> (resultValue.size()),
-		k
-	};
-
-    // Fill in the mask.
-	// TODO: make this non-specific to the representation.
-	std::unordered_set<std::string> inputValues;
-	for (auto x : input) inputValues.insert(x.value);
-	for (int i = 0; i <= resultValue.size() - k; ++i) {
-		if (inputValues.count(resultValue.substr(i, k))) {
-			result.mask[i] = true;
-		}
-	}
-
-	return result;
+int NucleotideToInt (char c) {
+    switch (c) {
+        case 'A': return 0;
+        case 'C': return 1;
+        case 'G': return 2;
+        case 'T': return 3;
+        default: throw std::invalid_argument("cannot convert letter " + std::string(1, c) + "to int.");
+    }
 }
 
+constexpr int INVALID_STATE = -1;
+struct ACState {
+    std::vector<int> supporters;
+    int forwardEdges[4] =  {INVALID_STATE, INVALID_STATE, INVALID_STATE, INVALID_STATE};
+    int backwardEdge = 0;
+    int depth = 0;
+    int id = 0;
+};
 
+struct ACAutomaton {
+    std::vector<ACState> states;
+    std::vector<int> endStateIndices;
+    std::vector<int> reversedOrdering;
+
+    int AddState(int depth) {
+        ACState newState;
+        newState.depth = depth;
+        newState.id = states.size();
+        states.push_back(newState);
+        return states.size() - 1;
+    }
+
+    void ConstructTrie(std::vector<KMer> kMers) {
+        endStateIndices = std::vector<int>(kMers.size());
+        // Initialize the root.
+        AddState(0);
+        for (int i = 0; i < kMers.size(); ++i) states[0].supporters.push_back(i);
+        // TODO: make this independent of the implementation by providing iterator to KMer.
+        for (int i = 0; i < kMers.size(); ++i) {
+            int state = 0;
+            for (char c : kMers[i].value) {
+                int index = NucleotideToInt(c);
+                if (states[state].forwardEdges[index] == INVALID_STATE) {
+                    int newState =  AddState(states[state].depth + 1);
+                    states[state].forwardEdges[index] = newState;
+                }
+                state = states[state].forwardEdges[index];
+                states[state].supporters.push_back(i);
+            }
+            endStateIndices[i] = state;
+        }
+
+        for (int i = 0; i < 4; ++i) {
+            if (states[0].forwardEdges[i] == INVALID_STATE) {
+                states[0].forwardEdges[i] = 0;
+            }
+        }
+    }
+
+    int Step (int state, int index) {
+        while (states[state].forwardEdges[index] == INVALID_STATE) {
+            state = states[state].backwardEdge;
+        }
+        return states[state].forwardEdges[index];
+    }
+
+    void ConstructBackwardEdges () {
+        reversedOrdering = std::vector<int> (states.size(), 0);
+        std::queue<int> q;
+        for (int i = 0; i < 4; ++i) {
+            if (states[0].forwardEdges[i] != 0) {
+                q.push(states[0].forwardEdges[i]);
+            }
+        }
+        int orderingIndex = reversedOrdering.size() - 2;
+        while (!q.empty()) {
+            int state = q.front();
+            reversedOrdering[orderingIndex--] = state;
+            q.pop();
+            for (int i = 0; i < 4; ++i) {
+                int nextState = states[state].forwardEdges[i];
+                if (nextState != INVALID_STATE && nextState != 0) {
+                    states[nextState].backwardEdge = Step(states[state].backwardEdge, i);
+                    q.push(nextState);
+                }
+            }
+        }
+    }
+
+    void Construct (std::vector<KMer> kMers) {
+        ConstructTrie(kMers);
+        ConstructBackwardEdges();
+    }
+};
+
+struct OverlapEdge {
+    int firstIndex;
+    int secondIndex;
+    int overlapLength;
+};
+
+std::vector<OverlapEdge> OverlapHamiltonianPath (std::vector<KMer> &kMers) {
+    ACAutomaton automaton;
+    automaton.Construct(kMers);
+    std::vector<bool> forbidden(kMers.size(), false);
+    std::vector<std::list<int>> incidentKMers(kMers.size());
+    std::vector<OverlapEdge> hamiltonianPath;
+    for (int s : automaton.reversedOrdering) {
+        // TODO: implement the kMer algorithm.
+    }
+    return hamiltonianPath;
+}
+
+std::string Suffix(KMer kMer, int overlap) {
+    return kMer.value.substr(overlap, kMer.length() - overlap);
+}
+
+KMerSet SuperstringFromPath(std::vector<OverlapEdge> &hamiltonianPath, std::vector<KMer> &kMers, int k) {
+    std::vector<OverlapEdge> edgeFrom (kMers.size(), OverlapEdge{-1,-1});
+    std::vector<bool> isStart(kMers.size(), true);
+    for (auto edge : hamiltonianPath) {
+        edgeFrom[edge.firstIndex] = edge;
+        isStart[edge.secondIndex] = false;
+    }
+
+    int start = 0;
+    for (; isStart[start] == false; ++start);
+
+    std::stringstream superstring;
+    superstring << kMers[start].value;
+    std::vector<bool> mask (1, 1);
+
+    while(edgeFrom[start].secondIndex != -1) {
+        superstring << Suffix(kMers[edgeFrom[start].secondIndex], edgeFrom[start].overlapLength);
+        for (int i = 0; i < k - 1 - edgeFrom[start].overlapLength; ++i) mask.push_back(0);
+        mask.push_back(1);
+        start = edgeFrom[start].secondIndex;
+    }
+
+    for(int i = 0; i < k - 1; ++i) mask.push_back(0);
+
+    return KMerSet {
+        superstring.str(),
+        mask,
+        k
+    };
+}
+
+/// Get the approximated shortest superstring of the given k-mers using the GREEDY algorithm.
+/// This runs in O(n).
+KMerSet Greedy(std::vector<KMer> &kMers) {
+	if (kMers.size() == 0) {
+		throw new std::invalid_argument("input cannot be empty");
+	}
+	int k = kMers[0].length();
+    auto hamiltonianPath = OverlapHamiltonianPath(kMers);
+    return SuperstringFromPath(hamiltonianPath, kMers, k);
+}
--- greedy_unittest.cpp
+++ greedy_unittest.cpp
@@ -3,30 +3,121 @@
 #include "gtest/gtest.h"
 
 namespace {
-    TEST(OverlapLengthTest, Trivial) {
-        EXPECT_EQ(2, OverlapLength(KMer{"ACGT"}, KMer{"GTAT"}));
-        EXPECT_EQ(3, OverlapLength(KMer{"ACCGT"}, KMer{"CGT"}));
-        EXPECT_EQ(3, OverlapLength(KMer{"CGT"}, KMer{"CGT"}));
-        EXPECT_EQ(3, OverlapLength(KMer{"ACCCTCCCA"}, KMer{"CCATAAAAAACCGGTTAA"}));
+    TEST(SuffixTest, Suffix) {
+        EXPECT_EQ("CGT", Suffix(KMer{"ACGT"}, 1));
+        EXPECT_EQ("CGTA", Suffix(KMer{"CGTA"}, 0));
+        EXPECT_EQ("", Suffix(KMer{"CC"}, 2));
     }
 
-    TEST(OverlapLengthTest, Zero) {
-        EXPECT_EQ(0, OverlapLength(KMer{"AAA"}, KMer{"TTT"}));
-        EXPECT_EQ(0, OverlapLength(KMer{"AAC"}, KMer{"TAA"}));
+    TEST(NucleotideToIntTest, NucleotideToInt) {
+        struct TestCase {
+            char nucleotide;
+            int wantResult;
+            bool wantSuccess;
+        };
+        std::vector<TestCase> tests = {
+                {'A', 0, true},
+                {'C', 1, true},
+                {'G', 2, true},
+                {'T', 3, true},
+                {'B', 0, false},
+        };
+
+        for (auto t : tests) {
+            int gotResult = 0;
+            bool gotSuccess = true;
+            try {
+                gotResult = NucleotideToInt(t.nucleotide);
+            } catch(std::invalid_argument) {
+                gotSuccess = false;
+            }
+            EXPECT_EQ(t.wantSuccess, gotSuccess);
+            if (t.wantSuccess) EXPECT_EQ(t.wantResult, gotResult);
+        }
     }
 
-    TEST(OverlapLengthTest, MultipleOccurrences) {
-        EXPECT_EQ(4, OverlapLength(KMer{"TACAC"}, KMer{"ACACGA"}));
-        EXPECT_EQ(2, OverlapLength(KMer{"GAA"}, KMer{"AAT"}));
+    TEST(SuperstringFromPathTest, SuperstringFromPath) {
+        struct TestCase {
+            std::vector<OverlapEdge> path;
+            std::vector<KMer> kMers;
+            int k;
+            KMerSet wantResult;
+        };
+        std::vector<TestCase> tests = {
+                {
+                    std::vector<OverlapEdge>{OverlapEdge{1, 0, 2},OverlapEdge{0, 2, 1}},
+                    std::vector<KMer>{KMer{"ACG"}, KMer{"TAC"}, KMer{"GGC"}},
+                    3,
+                    KMerSet{"TACGGC", std::vector<bool> {1, 1, 0, 1, 0, 0}, 3}
+                },
+        };
+
+        for (auto t : tests) {
+            KMerSet got = SuperstringFromPath(t.path, t.kMers, t.k);
+            EXPECT_EQ(t.wantResult.superstring, got.superstring);
+            EXPECT_EQ(t.wantResult.k, got.k);
+            EXPECT_EQ(t.wantResult.mask.size(), got.mask.size());
+            for (int i = 0; i < t.wantResult.mask.size(); ++i) {
+                EXPECT_EQ(t.wantResult.mask[i], got.mask[i]);
+            }
+        }
     }
 
-    TEST(Union, Union) {
-        EXPECT_EQ("AAAG", Union(KMer{"AAA"}, KMer{"AG"}, 1).value);
-        EXPECT_EQ("AAA", Union(KMer{"AAA"}, KMer{"AA"}, 2).value);
-        EXPECT_EQ("AAACTG", Union(KMer{"AAA"}, KMer{"CTG"}, 0).value);
+    TEST(ACAutomatonTest, ConstructTrie) {
+        ACAutomaton a;
+        std::vector<ACState> wantStates = {
+                ACState{std::vector<int>{0, 1}, 1, 0, 0, 0, 0, 0, 0},
+                ACState{std::vector<int>{0, 1}, -1, 2, -1, -1, 0, 1, 1},
+                ACState{std::vector<int>{0, 1}, -1, -1, 4, 3, 0, 2, 2},
+                ACState{std::vector<int>{0}, -1, -1, -1, -1, 0, 3, 3},
+                ACState{std::vector<int>{1}, -1, -1, -1, -1, 0, 3, 4},
+        };
+        std::vector<int> wantEndStateIndices = {3, 4};
+
+        a.ConstructTrie(std::vector<KMer>{KMer{"ACT"}, KMer{"ACG"}});
+
+        ASSERT_EQ(wantStates.size(), a.states.size());
+        for (int i = 0; i < wantStates.size(); ++i) {
+            EXPECT_EQ(wantStates[i].backwardEdge, a.states[i].backwardEdge);
+            for (int j = 0; j < 4; ++j) {
+                EXPECT_EQ(wantStates[i].forwardEdges[j], a.states[i].forwardEdges[j]);
+            }
+            EXPECT_EQ(wantStates[i].depth, a.states[i].depth);
+            EXPECT_EQ(wantStates[i].id, a.states[i].id);
+            EXPECT_EQ(wantStates[i].supporters, a.states[i].supporters);
+        }
+
+        ASSERT_EQ(wantEndStateIndices, a.endStateIndices);
     }
 
-    TEST(Greedy, Greedy) {
+    TEST(ACAutomatonTest, ConstructBackwardEdges) {
+        ACAutomaton a;
+        // Trie representing ["ACT", "ACA"].
+        a.states = {
+                ACState{std::vector<int>{0, 1}, 1, 0, 0, 0, 0, 0, 0},
+                ACState{std::vector<int>{0, 1}, -1, 2, -1, -1, 0, 1, 1},
+                ACState{std::vector<int>{0, 1}, 4, -1, -1, 3, 0, 2, 2},
+                ACState{std::vector<int>{0}, -1, -1, -1, -1, 0, 3, 3},
+                ACState{std::vector<int>{1}, -1, -1, -1, -1, 0, 3, 4},
+        };
+        std::vector<ACState> wantStates = {
+                ACState{std::vector<int>{0, 1}, 1, 0, 0, 0, 0, 0, 0},
+                ACState{std::vector<int>{0, 1}, -1, 2, -1, -1, 0, 1, 1},
+                ACState{std::vector<int>{0, 1}, 4, -1, -1, 3, 0, 2, 2},
+                ACState{std::vector<int>{0}, -1, -1, -1, -1, 0, 3, 3},
+                ACState{std::vector<int>{1}, -1, -1, -1, -1, 1, 3, 4},
+        };
+        std::vector<int> wantReversedOrdering = {3,4,2,1,0};
+
+        a.ConstructBackwardEdges();
+
+        for (int i = 0; i < wantStates.size(); ++i) {
+            EXPECT_EQ(wantStates[i].backwardEdge, a.states[i].backwardEdge);
+        }
+        EXPECT_EQ(wantReversedOrdering, a.reversedOrdering);
+    }
+
+    TEST(GreedyTest, Greedy) {
         std::vector<std::pair<KMerSet, std::vector<KMer>>> tests = {
                 {KMerSet{"TACGT",  std::vector<bool> {1, 1, 1, 0, 0}, 3 }, {KMer{"CGT"}, KMer{"TAC"}, KMer{"ACG"}}},
                 {KMerSet{"ACGTTT",  std::vector<bool> {1, 1, 0, 1, 0, 0}, 3 }, {KMer{"CGT"}, KMer{"TTT"}, KMer{"ACG"}}},
