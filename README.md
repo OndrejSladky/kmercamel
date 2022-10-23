1282d9d0eebaa382c205b5b355283777ac216e95
GordonHoklinder <ondra.sladky@gmail.com>
2022-10-23 13:36:39 +0200

    It is easier to run the program.

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
-## Usage
+## Algorithms
 
-To install the dependend fasta reader, execute `wget https://raw.githubusercontent.com/dancooke/bioio/master/bioio.hpp`.
+| Algorithm                     | Implemented | Basic optimizations | Support for complements |
+|-------------------------------|-------------|---------------------|-------------------------|
+| GREEDY with AC                | YES         | NO                  | NO                      |
+| GREEDY with hashing           | NO          | NO                  | NO                      |
+| Pseudosimplitigs with AC      | YES         | NO                  | NO                      |
+| Pseudosimplitigs with hashing | YES         | NO                  | NO                      |
+
+
+## How to install
+
+First clone the repo:
+
+```
+git clone https://github.com/GordonHoklinder/k-mers
+```
+
+Then download the dependend fasta reader:
+
+```
+wget https://raw.githubusercontent.com/dancooke/bioio/master/bioio.hpp
+```
+
+Compile the program:
+
+```
+g++ main.cpp -o kmers
+```
+
+## How to run
+
+As the first argument pass the path to the fasta file.
+
+Additionally you can pass other arguments:
+
+- `-k` -- the size of one k-mer. Default 13.
+- `-a` -- the algortihm which should be run. Either `greedy` for global GREEDY, `pseudosimplitigs` or `pseudosimplitigsAC` for greedily computing pseudosimplitigs.
+- The latter uses Aho-Corasick automaton. Default `greedy`.
+- `-d` -- d_max used in pseudosimplitigs. Default 5.
+
+For example:
+
+```
+./kmers ./spneumoniae.fa -a pseudosimplitigsAC -k 12 -d 7
+```
+
+runs on the pseudosimplitigs on the streptococcus fasta file with `k=12` and `d=7`.
+
--- main.cpp
+++ main.cpp
@@ -18,20 +18,51 @@ void WriteResult(KMerSet result, std::vector<KMer> kMers, std::string data, std:
     std::cout << "========================================="  << std::endl;
 }
 
+std::string GetFlag(char **begin, char **end, std::string param, std::string def) {
+    auto flag = std::find(begin, end, param);
+    if (flag != end && ++flag != end) return *flag;
+    return def;
+}
+
+int GetFlagAsInt(char **begin, char **end, std::string param, int def) {
+    auto ret = GetFlag(begin, end, param, "");
+    if (ret == "") return def;
+    return std::stoi(ret);
+}
+
+void Help() {
+    std::cerr << "Example usage:       ./kmers path_to_fasta -k 13 -d 5 -a greedy" << std::endl;
+    std::cerr << "Possible algorithms: greedy pseudosimplitigs pseudosimplitigsAC" << std::endl;
+}
+
 int main(int argc, char **argv) {
     std::string path;
     int k;
+    int d_max;
+    std::string algorithm;
     try {
         path = argv[1];
-        k = std::stoi(argv[2]);
+        k = GetFlagAsInt(argv, argv + argc, "-k", 13);
+        d_max = GetFlagAsInt(argv, argv + argc, "-d", 5);
+        algorithm = GetFlag(argv, argv + argc, "-a", "greedy");
     } catch (std::exception) {
-        std::cerr << "Expected usage: ./kmers path_to_fasta k" << std::endl;
+        Help();
         return 1;
     };
     auto data = bioio::read_fasta(path);
     for (auto record : data) {
         auto kMers = ConstructKMers(record.sequence, k);
-        auto result = GreedyGeneralizedSimplitigsAC(kMers, k, 3);
+        KMerSet result;
+        if (algorithm == "greedy")
+            result = Greedy(kMers);
+        else if (algorithm == "pseudosimplitigs")
+            result = GreedyGeneralizedSimplitigs(kMers, k, d_max);
+        else if (algorithm == "pseudosimplitigsAC")
+            result = GreedyGeneralizedSimplitigsAC(kMers, k, d_max);
+        else {
+            Help();
+            return 1;
+        }
         WriteResult(result, kMers, record.sequence, record.name);
         return 0;
     }
