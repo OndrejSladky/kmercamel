091e2248c21a24e7e723d45f06b0242392b61443
GordonHoklinder <ondra.sladky@gmail.com>
2022-11-04 15:48:21 +0100

    Algorithms are run on k-mers from all records.

--- parser.cpp
+++ parser.cpp
@@ -2,14 +2,23 @@
 #include <unordered_set>
 
 #include "models.h"
+#include "bioio.hpp"
 
 /// Create a list of unique k-mers in no particular order.
 /// This runs in O(k*data.size) expected time.
-std::vector<KMer> ConstructKMers(std::string data, int k) {
-    std::unordered_set<std::string> uniqueKMers;
-    if (size_t(k) > data.size()) return {};
+void AddKMersFromSequence(std::unordered_set<std::string> &kMers, std::string &data, int k) {
+    if (size_t(k) > data.size()) return;
     for (size_t i = 0; i <= data.size() - k; ++i) {
-        uniqueKMers.insert(data.substr(i, k));
+        kMers.insert(data.substr(i, k));
+    }
+}
+
+/// Create a list of unique k-mers in no particular order.
+/// This runs in O(k*data.size) expected time.
+std::vector<KMer> ConstructKMers(std::vector<bioio::FastaRecord<std::string, std::string>> &data, int k) {
+    std::unordered_set<std::string> uniqueKMers;
+    for (auto &&record : data) {
+        AddKMersFromSequence(uniqueKMers, record.sequence, k);
     }
     std::vector<KMer> result;
     for (auto kMer : uniqueKMers) {
