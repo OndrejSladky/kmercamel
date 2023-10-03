32ce7fe82e3d8c615a042e700716adcd466b3a21
OndrejSladky <ondra.sladky@seznam.cz>
2023-10-03 21:55:28 +0200

    [convert_superstring] improved help

--- convert_superstring.py
+++ convert_superstring.py
@@ -5,9 +5,11 @@ from typing import List
 import sys
 
 
+
+
 def split_superstring(superstring: str, k: int) -> List[str]:
     """
-    Split the given superstring into string where at every position (except for the last k-1) begins a k-mer.
+    Split the given superstring into strings where at every position (except for the last k-1) begins a k-mer.
     """
     ret: List[str] = []
     next_start = 0
@@ -41,7 +43,12 @@ def main():
     """
     Read the superstring from standard input and print the rSPSS representation.
     """
-    parser = argparse.ArgumentParser(description="Convert a masked superstring to its rSPSS representation")
+    parser = argparse.ArgumentParser(description="Convert a masked superstring to its rSPSS representation.",
+                                     usage="\nThe masked superstring is split at every position with 0 in the mask,\n"
+                                     "which results in matchtigs (rSPSS) containing the same set of k-mers\n"
+                                     "as was carried by the masked superstring (i.e. those k-mers masked with 1s)\n"
+                                     "furthermore, if the mask minimized number of ones, the result are simplitigs (SPSS)."
+                                     )
     parser.add_argument("infile", nargs="?", type=argparse.FileType("r"), default=sys.stdin)
     args = parser.parse_args()
 
