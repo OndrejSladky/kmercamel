4f64e0014b9daf4bd5e515bb3c2e617ef72c2cfc
OndrejSladky <ondra.sladky@seznam.cz>
2023-10-03 20:47:54 +0200

    [create_version] Six characters is all you need

--- create-version.sh
+++ create-version.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cat <(echo "#define VERSION \"") src/version <(echo -) <(git rev-parse HEAD) <(echo \") | tr -d '\n' > src/version.h
+cat <(echo "#define VERSION \"") src/version <(echo -) <(git rev-parse HEAD | head -c6) <(echo \") | tr -d '\n' > src/version.h
