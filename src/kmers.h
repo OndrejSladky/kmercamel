2fd8e087f758ebf344e3532fb6f40ccb615bf19a
OndrejSladky <ondra.sladky@seznam.cz>
2023-12-25 16:41:47 +0100

    [kmers.h] Reverse complements made efficient.

--- src/kmers.h
+++ src/kmers.h
@@ -41,15 +41,34 @@ int64_t BitSuffix(int64_t kMer, int d) {
     return kMer & ((1LL << (d << 1LL)) - 1LL);
 }
 
+/// Checkered mask. cmask<uint16_t, 1> is every other bit on
+/// (0x55). cmask<uint16_t,2> is two bits one, two bits off (0x33). Etc.
+/// Copyright: Jellyfish GPL-3.0
+template<typename U, int len, int l = sizeof(U) * 8 / (2 * len)>
+struct cmask {
+    static const U v =
+            (cmask<U, len, l - 1>::v << (2 * len)) | (((U)1 << len) - 1);
+};
+template<typename U, int len>
+struct cmask<U, len, 0> {
+    static const U v = 0;
+};
+
+/// Compute the reverse complement of a word.
+/// Copyright: Jellyfish GPL-3.0
+inline uint64_t word_reverse_complement(uint64_t w) {
+    typedef uint64_t U;
+    w = ((w >> 2)  & cmask<U, 2 >::v) | ((w & cmask<U, 2 >::v) << 2);
+    w = ((w >> 4)  & cmask<U, 4 >::v) | ((w & cmask<U, 4 >::v) << 4);
+    w = ((w >> 8)  & cmask<U, 8 >::v) | ((w & cmask<U, 8 >::v) << 8);
+    w = ((w >> 16) & cmask<U, 16>::v) | ((w & cmask<U, 16>::v) << 16);
+    w = ( w >> 32                   ) | ( w                    << 32);
+    return ((U)-1) - w;
+}
+
 /// Compute the reverse complement of the given k-mer.
 int64_t ReverseComplement(int64_t kMer, int k) {
-    int64_t ans = 0;
-    for (int i = 0; i < k; ++i) {
-        ans <<= 2;
-        ans |= 3 ^ (kMer & 3);
-        kMer >>= 2;
-    }
-    return ans;
+    return (((int64_t)word_reverse_complement(kMer)) >> (64LL - (k << 1LL))) & ((1LL << (k << 1LL)) - 1LL);
 }
 
 /// Return the complementary nucleotide for the given one.
