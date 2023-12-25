0b9fb6c46357a49109e9e1254f5592ed5c50c646
OndrejSladky <ondra.sladky@seznam.cz>
2023-12-25 16:11:30 +0100

    Reverse complements checked on the fly.

--- src/generalized_simplitigs_unittest.h
+++ src/generalized_simplitigs_unittest.h
@@ -10,20 +10,21 @@ namespace {
             std::unordered_set<int64_t> kMers;
             int k;
             int d;
+            bool complements;
             int64_t wantExt;
             int64_t  wantNext;
         };
         std::vector<TestCase> tests = {
                 // ACT; {TCC, CTA, ACT, CCT}; A; CTA
-                {0b000111, {0b110101, 0b011100, 0b000111, 0b010111}, 3, 1, 0b00, 0b011100},
+                {0b000111, {0b110101, 0b011100, 0b000111, 0b010111}, 3, 1, false, 0b00, 0b011100},
                 // ACT; {TCC, ACT, CCT}; CC; TCC
-                {0b111111, {0b110101, 0b000111, 0b010111}, 3, 2,   0b0101, 0b110101},
+                {0b111111, {0b110101, 0b000111, 0b010111}, 3, 2,   false, 0b0101, 0b110101},
                 // ACT; {TCC, ACT, CCT}
-                {0b000111, {0b110101, 0b000111, 0b010111}, 3, 1,    -1, -1},
+                {0b000111, {0b110101, 0b000111, 0b010111}, 3, 1,  false,  -1, -1},
         };
 
         for (auto t: tests) {
-            auto got = RightExtension(t.last, t.kMers, t.k, t.d);
+            auto got = RightExtension(t.last, t.kMers, t.k, t.d, t.complements);
             auto gotExt = got.first;
             auto gotNext = got.second;
             EXPECT_EQ(t.wantNext, gotNext);
@@ -37,18 +38,19 @@ namespace {
             std::unordered_set<int64_t> kMers;
             int k;
             int d;
+            bool complements;
             int64_t wantExt;
             int64_t  wantNext;
         };
         std::vector<TestCase> tests = {
                 // ACT; {TCC, ACT, CCT}
-                {0b000111, {0b110101, 0b000111, 0b010111}, 3, 1,    -1, -1},
+                {0b000111, {0b110101, 0b000111, 0b010111}, 3, 1, false,   -1, -1},
                 // TAC; {TCC, CTA, ACT, CCT}; C; CTA
-                {0b110001, {0b110101, 0b011100, 0b000111, 0b010111}, 3, 1,    0b01, 0b011100},
+                {0b110001, {0b110101, 0b011100, 0b000111, 0b010111}, 3, 1, false, 0b01, 0b011100},
         };
 
         for (auto t: tests) {
-            auto got = LeftExtension(t.first, t.kMers, t.k, t.d);
+            auto got = LeftExtension(t.first, t.kMers, t.k, t.d, t.complements);
             auto gotExt = got.first;
             auto gotNext = got.second;
             EXPECT_EQ(t.wantNext, gotNext);
